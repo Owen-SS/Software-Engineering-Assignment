5fe9e9cdca9b79a0ca47da09c0bca8e6fc595a9f
J-shw <91017208+J-shw@users.noreply.github.com>
2022-11-06 13:07:33 +0000

    Added edit/save features
    Now the buttons will enable and disable the text boxes

---
+++ static/js/student_script.js
@@ -0,0 +1,30 @@
+function editMode(edit) {
+    console.log("Ran")
+    console.log(edit)
+
+    var fName = document.getElementById("fName");
+    var sName = document.getElementById("sName");
+    var dob = document.getElementById("dob");
+    var addOne = document.getElementById("AD1");
+    var addTwo = document.getElementById("AD2");
+    var addThree = document.getElementById("AD3");
+    var postcode = document.getElementById("postcode");
+
+    if (edit.value == "Yes") {
+        fName.setAttribute("disabled", "disabled");
+        sName.setAttribute("disabled", "disabled");
+        dob.setAttribute("disabled", "disabled");
+        addOne.setAttribute("disabled", "disabled");
+        addTwo.setAttribute("disabled", "disabled");
+        addThree.setAttribute("disabled", "disabled");
+        postcode.setAttribute("disabled", "disabled");
+    } else {
+        fName.removeAttribute("disabled");
+        sName.removeAttribute("disabled");
+        dob.removeAttribute("disabled");
+        addOne.removeAttribute("disabled");
+        addTwo.removeAttribute("disabled");
+        addThree.removeAttribute("disabled");
+        postcode.removeAttribute("disabled");
+    }
+}
