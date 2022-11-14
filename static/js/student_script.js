2879544ff3f12857c3298c98ce272baa1c60106e
J-shw <91017208+J-shw@users.noreply.github.com>
2022-11-14 12:50:01 +0000

    Delete account function

--- static/js/student_script.js
+++ static/js/student_script.js
@@ -161,5 +161,32 @@ function updateDetails(){
 }
 
 function deleteAccount(){
-  let id = "Smithy1667832522128"
+  uploadObject = ["None"]
+
+  let url = "/delete/account";
+  let xhttp = new XMLHttpRequest();
+  let data_res = "Error"
+  xhttp.onreadystatechange = function() {
+    let strResponse = "Error: no response";
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("here")
+      strResponse = JSON.parse(this.responseText);
+      data_res = strResponse.data
+
+      console.log(data_res)
+      if (strResponse.message != 404){
+        displaydetails(data_res)
+      }else{
+        alert("Whoops somthing went wrong!")
+        console.log("Get details error - " + strResponse.message)
+      }
+    }
+  };
+  xhttp.open("PUT", url, true);
+  // Converting JSON data to string
+  var data = JSON.stringify(uploadObject);
+  // Set the request header i.e. which type of content you are sending
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  //send it
+  xhttp.send(data);
 }
