8f636b04c31fd0973e32ffa90c1c5517c4fc6d6b
Owen Staddon <Owen.Staddon@icloud.com>
2022-11-18 17:45:18 +0000

    Added Sql alchemy database this is using sqlite
    I have only added the database i have not changed any of the existing code

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from flask import Flask, render_template, jsonify, request, make_response, session
 import sys, json, os, csv
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import pandas as pd
+
+
+
 app = Flask('app')
 #This secret key is needed for session
 app.secret_key = "oiahjds9fuhaushdfuygasducnjxzn"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key= True)
+  username = db.Column(db.String(20), unique = True, nullable = False )
+  email = db.Column(db.String(120), unique = True, nullable = False )
+  image_file = db.Column(db.String(20), nullable = False, default= 'default.jpg')
+  password = db.Column(db.String(60), nullable = False)
+  posts = db.relationship('Post', backref='author',lazy= True)
+
+
+  def __repr__(self):
+    return f"User ('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key= True)
+  title = db.Column(db.String(100), nullable = False)
+  date_posted = db.Column(db.DateTime,nullable=False, default = datetime.utcnow)
+  content = db.Column(db.Text, nullable = False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+  def __repr__(self):
+    return f"Post ('{self.title}', '{self.date_posted}', '{self.content}')"
+
+
+
 
 @app.route('/')
 def hello_world():
@@ -232,6 +264,7 @@ def deleteAccount():
     return messageOK
   else:
     return messageFail
+
 
 
 app.run(host='0.0.0.0', port=8080)
