8934b803e51b24a0e5d029acdb75e67f6aee2545
J-shw <91017208+J-shw@users.noreply.github.com>
2022-12-01 22:51:56 +0000

    added new text + fixed IDs

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,22 @@
 function getDetails(){
-  uploadObject = ["None"]
+  uploadObject = ["None"];
 
   let url = "/displaydetails";
   let xhttp = new XMLHttpRequest();
-  let data_res = "Error"
+  let data_res = "Error";
   xhttp.onreadystatechange = function() {
     let strResponse = "Error: no response";
     if (this.readyState == 4 && this.status == 200) {
       strResponse = JSON.parse(this.responseText);
-      data_res = strResponse.data
+      data_res = strResponse.data;
 
-      console.log(data_res)
+      console.log(data_res);
       if (strResponse.message != 404){
-        displaydetails(data_res)
-        addJob()
+        displaydetails(data_res);
+        addJob();
       }else{
-        alert("Whoops somthing went wrong!")
-        console.log("Get details error - " + strResponse.message)
+        alert("Whoops somthing went wrong!");
+        console.log("Get details error - " + strResponse.message);
       }
     }
   };
@@ -38,22 +38,21 @@ function displaydetails(data){
 function addElemen(list){
   const element = document.getElementById("job-list");
 
-  const idName = ['job-name', 'start-date', 'salary', 'location', 'job-desc', 'contact']
-  for(mainIndex in list){
+  const idName = ['job-name', 'contract-type', 'start-date', 'salary','location', 'job-desc', 'email'];
+  const textB = ['', '', '', '£','', '', ''];
+  for(mainIndex in list) {
     let div = document.createElement("div");
-    div.id = "job"
-    let id = list[mainIndex][0]
+    div.id = "job";
+    let id = list[mainIndex][0];
 
     for(index in list[mainIndex]){
-      
 
-      let text = list[mainIndex][index]
-
+      let text = list[mainIndex][index];
       const para = document.createElement("p");
-      const node = document.createTextNode(text);
-      para.id = idName[index]
+      const node = document.createTextNode(textB[index] + text);
+      para.id = idName[index];
       para.appendChild(node);
-      div.appendChild(para)
+      div.appendChild(para);
     }
     element.appendChild(div);
   }
